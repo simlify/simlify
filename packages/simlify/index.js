57e47f034b5f001713862bf5c40642639a49c7a5
Christian Bühlmeyer <christian_buehlmeyer@yahoo.de>
2019-09-18 23:15:28 +0200

    sharing one express instance between client and server

--- packages/simlify/index.js
+++ packages/simlify/index.js
@@ -1,5 +1,12 @@
 const client = require('@simlify/client');
 const server = require('@simlify/server');
+const express = require('express');
 
-client.start();
-server.start();
+const app = express();
+
+server.start(app)
+  .then(() => {
+    client.start(app);
+    app.listen(8080);
+})
+
