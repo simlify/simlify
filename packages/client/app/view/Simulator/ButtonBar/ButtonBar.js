0f97e225977c4ce9c5f2a93122cde358c6442007
Christian Bühlmeyer <christian_buehlmeyer@yahoo.de>
2019-09-28 12:02:02 +0200

    Making own component for ButtonBar

---
+++ packages/client/app/view/Simulator/ButtonBar/ButtonBar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Button from '@material-ui/core/Button';
+import Menu from '../Menu';
+import { flowActions, nodeActions } from '../../../store/actions';
+
+import './ButtonBar.scss';
+
+export default function ButtonBar({ nodeAreaRef }) {
+  const { flows, currentFlowIndex } = useSelector(state => state.flowData);
+  const dispatch = useDispatch();
+  const currentFlow = flows[currentFlowIndex];
+
+  const sendFlow = () => {
+    const serializedFlow = nodeAreaRef.current.serialize();
+    dispatch(flowActions.sendFlow(serializedFlow));
+  }
+
+  const loadFlow = () => {
+    dispatch(flowActions.loadFlow());
+  }
+  
+  return (
+    <div className="buttonBar">
+      <div className="buttonContainer"/>
+      <div className="buttonContainer">
+        <ButtonGroup variant="contained" size="small" aria-label="small contained button group">
+          <Button onClick={() => loadFlow()}>Reset</Button>
+          <Button onClick={() => sendFlow()}>Save</Button>
+        </ButtonGroup>
+      </div>
+      <Menu
+        className="buttonContainer buttonContainer__right"
+        nodeArea={nodeAreaRef.current}
+        currentFlow={currentFlow}
+      />
+    </div>
+  );
+};
--- packages/client/app/view/Simulator/Simulator.js
+++ packages/client/app/view/Simulator/Simulator.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
-import Button from '@material-ui/core/Button';
+import { flowActions, nodeActions } from '../../store/actions';
 import NodeArea from '../../components/NodeArea';
 import TabBar from '../../components/TabBar';
-import DropDownButton from '../../components/DropDownButton';
-import api from '../../../helper/api';
 import SideHelper from '../../components/SideHelper';
-import Menu from './Menu';
-import { flowActions, nodeActions } from '../../store/actions';
+import ButtonBar from './ButtonBar';
 
 import './Simulator.scss';
 
@@ -19,25 +15,8 @@ class Simulator extends React.Component {
   }
 
   UNSAFE_componentWillMount() {
-    this.loadAvailableNodes();
-    this.loadFlows();
-  }
-
-  loadFlows() {
-    this.props.dispatch(flowActions.loadFlow());
-  }
-
-  loadAvailableNodes() {
     this.props.dispatch(nodeActions.loadAvailableNodes());
-  }
-
-  getFlowSerialized() {
-    return this.nodeAreaRef.current.serialize();
-  }
-
-  sendFlow() {
-    const serializedFlow = this.getFlowSerialized();
-    this.props.dispatch(flowActions.sendFlow(serializedFlow));
+    this.props.dispatch(flowActions.loadFlow());
   }
 
   onTabChange(tabIndex) {
@@ -70,20 +49,9 @@ class Simulator extends React.Component {
     return (
       <div className="simulator">
         <div className="simulator__nodeArea">
-          <div className="simulator__buttonBar">
-            <div className="buttonContainer"/>
-            <div className="buttonContainer">
-              <ButtonGroup variant="contained" size="small" aria-label="small contained button group">
-                <Button onClick={() => this.loadFlows()}>Reset</Button>
-                <Button onClick={() => this.sendFlow()}>Save</Button>
-              </ButtonGroup>
-            </div>
-            <Menu
-              className="buttonContainer buttonContainer__right"
-              nodeArea={this.nodeAreaRef.current}
-              currentFlow={currentFlow}
-            />
-          </div>
+          <ButtonBar 
+            nodeAreaRef={this.nodeAreaRef}
+          />
           <TabBar
             tabs={tabs}
             onTabChange={(selectedTab) => this.onTabChange(selectedTab)}
@@ -103,12 +71,6 @@ class Simulator extends React.Component {
   }
 };
 
-function mapStateToProps(state) {
-  const { flowData, nodeData } = state;
-  return {
-    flowData,
-    nodeData,
-  };
-}
+const mapStateToProps = ({ flowData, nodeData }) => ({ flowData, nodeData });
 
 export default connect(mapStateToProps)(Simulator);
