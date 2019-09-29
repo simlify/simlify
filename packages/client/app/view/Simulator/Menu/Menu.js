1219080836704676b2d498d1dfe6b6881ddc5c4e
Christian Bühlmeyer <christian_buehlmeyer@yahoo.de>
2019-09-29 12:17:06 +0200

    Adding Snackbar message for successfull Import

--- packages/client/app/view/Simulator/Menu/Menu.js
+++ packages/client/app/view/Simulator/Menu/Menu.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import DropDownButton from 'components/DropDownButton';
 import convertForApi from 'helper/convertForApi';
+import { alertActions } from 'store/actions';
 
 import './Menu.scss';
 
 export default function Menu(props) {
+  const dispatch = useDispatch();
+
   const importFlow = () => {
     props.onImportClick();
   };
+
   const exportFlow = () => {
     const { nodeArea, currentFlow } = props;
     const flowSerialized = nodeArea.serialize();
     const flowSerializedForExport = convertForApi.convertFlow(flowSerialized);
     flowSerializedForExport.name = currentFlow.name;
     navigator.clipboard.writeText(JSON.stringify(flowSerializedForExport));
+    dispatch(alertActions.success('Exported flow to clipboard'));
   };
 
   const items = [
