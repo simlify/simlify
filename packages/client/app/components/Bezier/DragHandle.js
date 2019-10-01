7717deb907e27c9f151f989af1b89aea2b941399
Christian Bühlmeyer <christian_buehlmeyer@yahoo.de>
2019-10-01 21:11:47 +0200

    Bugfix: Exchanged Axis for DragHandler of BezierCurve

--- packages/client/app/components/Bezier/DragHandle.js
+++ packages/client/app/components/Bezier/DragHandle.js
@@ -1,66 +1,90 @@
 import React, { useState } from 'react';
 
-const DragHandle = (props) => {
-  const {
+const DragHandlerLine = ({
+  selected,
+  strokeVector,
+  handleStroke = 3,
+  dragHandleColor = '#00c0ff',
+}) => {
+  return(
+    <line
+      stroke={dragHandleColor}
+      strokeWidth={selected ? 1 + handleStroke : handleStroke}
+      x1={strokeVector[0]}
+      y1={strokeVector[1]}
+      x2={strokeVector[2]}
+      y2={strokeVector[3]}
+    />
+  )
+}
+
+const DragHandlerCircle = ({
+  selected,
+  onMouseDown,
+  onMouseUp,
+  setHover,
+  strokeVector,
+  handleStroke = 3,
+  handleRadius = 5,
+  dragHandleColor = '#00c0ff',
+}) => {
+  return (
+    <circle
+      cx={strokeVector[2]}
+      cy={strokeVector[3]}
+      r={handleRadius}
+      stroke={dragHandleColor}
+      strokeWidth={selected ? 2 * handleStroke : handleStroke}
+      fill={dragHandleColor}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      onMouseDown={onMouseDown}
+      onMouseUp={onMouseUp}
+    />
+  )
+}
+
+const DragHandle = ({
     right,
     bottom,
     width,
     height,
+    onMouseUp: propOnMouseUp,
+    onMouseDown: propOnMouseDown,
     position = [0.2, 0.2],
-  } = props;
-
-  const dragHandleColor = '#00c0ff';
+}) => {
   const [hover, setHover] = useState(false);
   const [down, setDown] = useState(false);
-  const handleStroke = 3;
-  const handleRadius = 5;
 
   function relativeToAbsolutePos(relativePosition) {
     return [relativePosition[0] * width, height - relativePosition[1] * height];
   }
 
-  let strokeOriginX = 0;
-  let strokeOriginY = 0;
-  let [strokeDestinationX, strokeDestinationY] = relativeToAbsolutePos(position);
+  const strokeOriginX = right ? width : 0;
+  const strokeOriginY = bottom ? height : 0;
+  const [strokeDestinationX, strokeDestinationY] = relativeToAbsolutePos(position);
 
-  if(bottom) {
-    strokeOriginY = height;
-  }
-  if(right) {
-    strokeOriginX = width;
-  }
+  const strokeVector = [strokeOriginX, strokeOriginY, strokeDestinationX, strokeDestinationY];
 
-  const onMouseEnter = () => setHover(true);
-  const onMouseLeave = () => setHover(false);
   const onMouseUp = () => {
     setDown(false);
-    props.onMouseUp();
+    propOnMouseUp();
   };
+
   const onMouseDown = () => {
     setDown(true);
-    props.onMouseDown();
+    propOnMouseDown();
   };
 
   return(
     <g>
-      <line
-        stroke={dragHandleColor}
-        strokeWidth={hover||down ? 1 + handleStroke : handleStroke}
-        x1={strokeOriginX}
-        y1={strokeOriginY}
-        x2={strokeDestinationX}
-        y2={strokeDestinationY} />
-      <circle
-        cx={strokeDestinationX}
-        cy={strokeDestinationY}
-        r={handleRadius}
-        stroke={dragHandleColor}
-        strokeWidth={hover||down ? 2 * handleStroke : handleStroke}
-        fill={dragHandleColor}
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
+      <DragHandlerLine selected={hover||down} strokeVector={strokeVector} />
+      <DragHandlerCircle
+        selected={hover||down}
         onMouseDown={onMouseDown}
         onMouseUp={onMouseUp}
+        setHover={setHover}
+        strokeVector={strokeVector}
       />
     </g>
   );
