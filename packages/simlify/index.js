6c62d7929487639cd1c0acd241d87943eac4aa5d
Christian Bühlmeyer <christian_buehlmeyer@yahoo.de>
2019-10-20 09:31:52 +0200

    Using httpsServer in similfy package for socketio implementation

--- packages/simlify/index.js
+++ packages/simlify/index.js
@@ -1,12 +1,17 @@
 const client = require('@simlify/client');
 const server = require('@simlify/server');
 const express = require('express');
+const http = require('http');
+
+const PORT = 8080;
 
 const app = express();
+const httpServer = http.Server(app);
 
-server.start(app)
+server.start(app, httpServer)
   .then(() => {
     client.start(app);
-    app.listen(8080);
+    httpServer.listen(PORT, () => {
+      console.log(`Application running on Port ${PORT}`);
+    });
 })
-
